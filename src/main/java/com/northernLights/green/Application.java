abe43151e69033765def312e8419a600f3f77889
9045902864 <68778796+9045902864@users.noreply.github.com>
2020-08-10 14:23:24 +0530

    initial commit
    very first commit

---
+++ src/main/java/com/northernLights/green/Application.java
@@ -0,0 +1,13 @@
+package com.northernLights.green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
