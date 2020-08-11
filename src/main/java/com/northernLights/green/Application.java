3eaf83e8e698090d03cc9a772cf89fc12977460f
9045902864 <68778796+9045902864@users.noreply.github.com>
2020-08-11 17:33:07 +0530

    cross origin

--- src/main/java/com/northernLights/green/Application.java
+++ src/main/java/com/northernLights/green/Application.java
@@ -3,9 +3,11 @@ package com.northernLights.green;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableSwagger2
+@CrossOrigin
 public class Application {
 
 	public static void main(String[] args) {
